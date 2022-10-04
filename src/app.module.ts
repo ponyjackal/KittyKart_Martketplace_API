a196627ba1c6fe63e2ce86d544b631dcf14077ed
benguyenkitty <benjamin@9vrse.org>
2022-10-04 10:07:51 -0800

    feat: add prisma module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { AccountModule } from './account/account.module';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [AuthModule, AccountModule],
+  imports: [AuthModule, AccountModule, PrismaModule],
   controllers: [AppController],
   providers: [AppService],
 })
