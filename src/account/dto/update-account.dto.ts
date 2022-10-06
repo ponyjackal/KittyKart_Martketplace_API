724b789fbe0892b7dd0d1d2b3f43c2f69c7425f3
ponyjackal <pony1125@outlook.com>
2022-10-05 16:16:13 -0800

    feat: update account service

--- src/account/dto/update-account.dto.ts
+++ src/account/dto/update-account.dto.ts
@@ -1,4 +1,3 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateAccountDto } from './create-account.dto';
-
-export class UpdateAccountDto extends PartialType(CreateAccountDto) {}
+export class UpdateAccountDto {
+    username: string;
+}
