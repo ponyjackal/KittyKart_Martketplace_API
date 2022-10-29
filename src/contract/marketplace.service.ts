1bfe33cf158343381a591936cb4155b73ae87047
ponyjackal <pony1125@outlook.com>
2022-10-29 08:14:42 -0700

    feat: add tx signature verification

--- src/contract/marketplace.service.ts
+++ src/contract/marketplace.service.ts
@@ -13,19 +13,11 @@ export class MarketplaceService {
         this.contract = this.ethersProvider.create(process.env.MARKETPLACE_ADDRESS, ABI);
     }
 
-    async list(contractAddress: string, tokenId: number, price: number) {
-        return await this.contract.list(contractAddress, tokenId, price);
+    async getNonce(address: string): Promise<number> {
+        return await this.contract.nonce(address);
     }
 
-    async buy(contractAddress: string, tokenId: number, amount: number) {
-        return await this.contract.buy(contractAddress, tokenId, amount);
-    }
-
-    async makeOffer(contractAddress: string, tokenId: number, amount: number) {
-        return await this.contract.makeOffer(contractAddress, tokenId, amount);
-    }
-
-    async acceptOffer(contractAddress: string, tokenId: number) {
-        return await this.contract.acceptOffer(contractAddress, tokenId);
+    async isSignatureVerified(signature: string): Promise<boolean> {
+        return await this.contract.signatures(signature);
     }
 }
