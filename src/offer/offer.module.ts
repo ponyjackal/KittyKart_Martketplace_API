1432840d83ec80a2300ed5cd1f8bfb45e49166b0
benguyenkitty <benjamin@9vrse.org>
2022-10-29 08:14:42 -0700

    feat: add tx signature verification

--- src/offer/offer.module.ts
+++ src/offer/offer.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { OfferService } from './offer.service';
 import { OfferController } from './offer.controller';
-import { PrismaModule } from 'src/prisma/prisma.module';
+import { PrismaModule } from '../prisma/prisma.module';
+import { ContractModule } from '../contract/contract.module';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule, ContractModule],
   providers: [OfferService],
   controllers: [OfferController],
   exports: [OfferService],
--- src/offer/offer.service.ts
+++ src/offer/offer.service.ts
@@ -1,23 +1,30 @@
 import { Injectable, BadRequestException } from '@nestjs/common';
 import { verifyMessage } from 'nestjs-ethers';
 import { PrismaService } from 'src/prisma/prisma.service';
+import { MarketplaceService } from '../contract/marketplace.service';
 import { CreateOfferDto } from './dto/create-offer.dto';
 import { checksumAddress } from '../utils/checkSumAddress';
 @Injectable()
 export class OfferService {
     constructor(
-        private prisma: PrismaService
+        private prisma: PrismaService,
+        private marketplaceService: MarketplaceService
     ) {}
 
-    create(address: string, createOfferDto: CreateOfferDto) {
+    async create(address: string, createOfferDto: CreateOfferDto) {
         // validate user address and signature
-        const message = process.env.MESSAGE;
+        const nonce = await this.marketplaceService.getNonce(address);
+        const message = process.env.MESSAGE + nonce;
         const derivedAddress = verifyMessage(message, createOfferDto.ownerSignature);
         if (checksumAddress(derivedAddress) !== checksumAddress(address)) {
-            throw new BadRequestException('Invalid signature');
+            throw new BadRequestException('Invalid user signature');
         }
         // validate typed signature
-        
+        const isSignatureVerified = await this.marketplaceService.isSignatureVerified(createOfferDto.txSignature);
+        if(!isSignatureVerified) {
+            throw new BadRequestException('Invalid tx signature');
+        }
+        // create offer
     }
 
     findAll() {
