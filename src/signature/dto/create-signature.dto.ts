07638781b53c0241085a9a83b8424f15990fd33f
adelinkity <adelin@9vrse.org>
2022-11-08 07:02:17 -0600

    feat: add swagger doc

--- src/signature/dto/create-signature.dto.ts
+++ src/signature/dto/create-signature.dto.ts
@@ -1,10 +1,22 @@
+import { ApiProperty } from '@nestjs/swagger';
+
+export enum ActionType {
+  LIST = 0,
+  BUY = 1,
+  OFFER = 2,
+  ACCEPT_OFFER = 3,
+}
+
 export class CreateSignatureDto {
+  @ApiProperty()
   collection: string;
+
+  @ApiProperty()
   tokenId: number;
+
+  @ApiProperty()
   price: number;
-  actionType: number;
-  // 0: LIST
-  // 1: BUY
-  // 2: OFFER
-  // 3: ACCEPT_OFFER
+
+  @ApiProperty()
+  actionType: ActionType;
 }
