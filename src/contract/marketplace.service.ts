6c32539810c26faf1f4f39ec166940e8600f304e
ponyjackal <pony1125@outlook.com>
2022-11-11 11:47:03 -0800

    feat: add logger for contract services

--- src/contract/marketplace.service.ts
+++ src/contract/marketplace.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, BadRequestException } from '@nestjs/common';
+import { Injectable, BadRequestException, Logger } from '@nestjs/common';
 import {
   EthersContract,
   InjectContractProvider,
@@ -8,6 +8,7 @@ import ABI from './abis/KittyKartMarketplace.json';
 
 @Injectable()
 export class MarketplaceService {
+  private readonly logger = new Logger(MarketplaceService.name);
   private contract: Contract;
 
   constructor(
@@ -21,30 +22,45 @@ export class MarketplaceService {
   }
 
   async getNonce(address: string): Promise<number> {
+    this.logger.log(`getNonce address: ${address}`);
     return await this.contract.nonces(address);
   }
 
   async isSignatureVerified(signature: string): Promise<boolean> {
+    this.logger.log(`isSingatureVerified`);
     try {
       return await this.contract.signatures(signature);
     } catch (error) {
+      this.logger.log(`invalid signature`);
       throw new BadRequestException('Invalid tx signature');
     }
   }
 
   async list(contractAddress: string, tokenId: number, price: number) {
+    this.logger.log(
+      `list contractAddress: ${contractAddress}, tokenId: ${tokenId}, price: ${price}`,
+    );
     return await this.contract.list(contractAddress, tokenId, price);
   }
 
   async buy(contractAddress: string, tokenId: number, amount: number) {
+    this.logger.log(
+      `buy contractAddress: ${contractAddress}, tokenId: ${tokenId}, amount: ${amount}`,
+    );
     return await this.contract.buy(contractAddress, tokenId, amount);
   }
 
   async makeOffer(contractAddress: string, tokenId: number, amount: number) {
+    this.logger.log(
+      `buy contractAddress: ${contractAddress}, tokenId: ${tokenId}, amount: ${amount}`,
+    );
     return await this.contract.makeOffer(contractAddress, tokenId, amount);
   }
 
   async acceptOffer(contractAddress: string, tokenId: number) {
+    this.logger.log(
+      `buy contractAddress: ${contractAddress}, tokenId: ${tokenId}`,
+    );
     return await this.contract.acceptOffer(contractAddress, tokenId);
   }
 }
