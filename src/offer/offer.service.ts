6e58860844cb981e19b62b371640bb6f66b41840
ponyjackal <pony1125@outlook.com>
2022-11-11 12:03:28 -0800

    feat: add a logger for offer service

--- src/offer/offer.service.ts
+++ src/offer/offer.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, BadRequestException } from '@nestjs/common';
+import { Injectable, BadRequestException, Logger } from '@nestjs/common';
 import { Auth, OFFER_STATUS } from '@prisma/client';
 import { verifyMessage } from 'nestjs-ethers';
 import { PrismaService } from '../prisma/prisma.service';
@@ -8,25 +8,37 @@ import { checksumAddress } from '../utils/checkSumAddress';
 
 @Injectable()
 export class OfferService {
+  private readonly logger = new Logger(OfferService.name);
+
   constructor(
     private prisma: PrismaService,
     private marketplaceService: MarketplaceService,
   ) {}
 
   async create(authUser: Auth, offerDto: OfferDto) {
+    this.logger.log(
+      `create a new offer; address: ${
+        authUser.address
+      }, offer: ${JSON.stringify(offerDto)}`,
+    );
     // validate user address and signature
+    this.logger.log('validating a user signature');
     const message = process.env.WALLET_SIGN_MESSAGE;
     const derivedAddress = verifyMessage(message, offerDto.ownerSignature);
     if (checksumAddress(derivedAddress) !== checksumAddress(authUser.address)) {
+      this.logger.log('user signature is invalid');
       throw new BadRequestException('Invalid user signature');
     }
     // validate typed signature
+    this.logger.log('validating a tx signature');
     const isSignatureVerified =
       await this.marketplaceService.isSignatureVerified(offerDto.txSignature);
     if (!isSignatureVerified) {
+      this.logger.log('invalid tx signature');
       throw new BadRequestException('Invalid tx signature');
     }
     // create offer
+    this.logger.log('creating an offer into db; offer');
     const offer = await this.prisma.offer.create({
       data: {
         requester: {
@@ -49,19 +61,27 @@ export class OfferService {
   }
 
   async accept(address: string, offerDto: OfferDto) {
-    // validate user address and signature
+    this.logger.log(
+      `accept an offer; address: ${address}, offerDto: ${offerDto}`,
+    );
+    // validate user address and signature\
+    this.logger.log('validating a user signature');
     const message = process.env.WALLET_SIGN_MESSAGE;
     const derivedAddress = verifyMessage(message, offerDto.ownerSignature);
     if (checksumAddress(derivedAddress) !== checksumAddress(address)) {
+      this.logger.log('user signature is invalid');
       throw new BadRequestException('Invalid user signature');
     }
     // validate typed signature
+    this.logger.log('validating a tx signature');
     const isSignatureVerified =
       await this.marketplaceService.isSignatureVerified(offerDto.txSignature);
     if (!isSignatureVerified) {
+      this.logger.log('invalid tx signature');
       throw new BadRequestException('Invalid tx signature');
     }
     // update accepted offer
+    this.logger.log(`updating offer in db`);
     const collection = await this.prisma.collection.findUnique({
       where: {
         address: offerDto.collectionAddress,
@@ -102,6 +122,7 @@ export class OfferService {
   }
 
   findOffers(collection: string, id: number) {
+    this.logger.log(`finding offers; collection: ${collection}, id: ${id}`);
     return this.prisma.offer.findMany({
       where: {
         collection_address: checksumAddress(collection),
@@ -111,10 +132,12 @@ export class OfferService {
   }
 
   findAll() {
+    this.logger.log('find all');
     return this.prisma.offer.findMany({});
   }
 
   findOne(id: number) {
+    this.logger.log(`find one; id: ${id}`);
     return this.prisma.offer.findUnique({ where: { id } });
   }
 }
