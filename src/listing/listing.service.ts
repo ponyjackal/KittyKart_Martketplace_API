89b7249c823886eb78ff7f38e807e9f22bcc6943
ponyjackal <pony1125@outlook.com>
2022-11-11 11:51:21 -0800

    feat: add logger for listing service

--- src/listing/listing.service.ts
+++ src/listing/listing.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, BadRequestException } from '@nestjs/common';
+import { Injectable, BadRequestException, Logger } from '@nestjs/common';
 import { LISTING_STATUS } from '@prisma/client';
 import { verifyMessage } from 'nestjs-ethers';
 import { PrismaService } from '../prisma/prisma.service';
@@ -8,38 +8,54 @@ import { checksumAddress } from '../utils/checkSumAddress';
 
 @Injectable()
 export class ListingService {
+  private readonly logger = new Logger(ListingService.name);
+
   constructor(
     private prisma: PrismaService,
     private marketplaceService: MarketplaceService,
   ) {}
 
   async create(address: string, listingDto: ListingDto) {
+    this.logger.log(
+      `creating a new list; address: ${address} list: ${JSON.stringify(
+        listingDto,
+      )}`,
+    );
+    this.logger.log(`validating signature`);
     // validate typed signature
     const isSignatureVerified =
       await this.marketplaceService.isSignatureVerified(listingDto.txSignature);
+
     if (!isSignatureVerified) {
+      this.logger.error(`invalid signature`);
       throw new BadRequestException('Invalid tx signature');
     }
 
+    const data = {
+      collection_address: listingDto.collectionAddress,
+      token_id: listingDto.tokenId,
+      price: listingDto.price,
+      seller_address: listingDto.ownerAddress,
+      status: LISTING_STATUS.ACTIVE,
+    };
+    this.logger.log(
+      `creating a new list into db; list: ${JSON.stringify(data)}`,
+    );
     // create listing
     const listing = await this.prisma.listing.create({
-      data: {
-        collection_address: listingDto.collectionAddress,
-        token_id: listingDto.tokenId,
-        price: listingDto.price,
-        seller_address: listingDto.ownerAddress,
-        status: LISTING_STATUS.ACTIVE,
-      },
+      data,
     });
 
     return listing;
   }
 
   findAll() {
+    this.logger.log('find all');
     return this.prisma.listing.findMany({});
   }
 
   findOne(id: number) {
+    this.logger.log('find one');
     return this.prisma.listing.findUnique({ where: { id } });
   }
 }
